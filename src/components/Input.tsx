39a58a35541731b5c1a5a2716a9086963e120b7b
David-Koshkadze <rootUS3rr@pm.me>
2022-07-04 22:03:25 +0400

    working on inputs

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,44 +1,27 @@
-import React, { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useField } from "formik";
 import green_allow_icon from "../assets/icons/green_allow_icon.svg";
 
-type InputProps = {
-  type: string;
-  name: string;
-  label: string;
-  required?: boolean;
-};
-
-const Input = ({ label, ...props }: InputProps) => {
-  const [value, setValue] = useState("");
+const Input = (props: any) => {
   const [isText, setIsText] = useState(false);
   const [isValid, setIsValid] = useState(false);
 
   const [field, meta] = useField(props);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-
-    if (event.target.value.length > 0) {
-      setIsText(true);
-    } else {
-      setIsText(false);
-    }
-  };
+  console.log(field.value);
+  useEffect(() => console.log("Rendered"), []);
 
   return (
     <div className="relative">
       <input
         {...props}
-        {...field}
-        value={value}
+        
         className="w-full px-4 py-2 text-black outline-none border-b-[1px] hover:bg-gray-50 focus:bg-[#E9ECEF] rounded-[4px] shadow-sm"
-        onChange={handleChange}
       />
 
-      {!isText && (
+      {isText && (
         <label htmlFor="input" className="absolute top-2 left-4">
-          {label} <span className="text-red-600">*</span>
+          {props.label} <span className="text-red-600">*</span>
         </label>
       )}
 
